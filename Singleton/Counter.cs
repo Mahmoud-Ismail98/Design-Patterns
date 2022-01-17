ba418734eb9c42dcec25235ccbc939fbe88c1c17
Mahmoud-Koraim <m.koraim77@gmail.com>
2022-01-17 20:25:46 +0200

    Singleton Pattern - Lazy Initialization Implementation

--- Singleton/Counter.cs
+++ Singleton/Counter.cs
@@ -4,13 +4,14 @@ namespace DesignPatterns.singleton
     public class Counter
     {
         public int Count = 0;
-        private static Counter instance = new Counter();
+        private static Counter instance = null;
         private Counter()
         {
 
         }
         public static Counter GetInstance()
         {
+            if (instance == null) { instance = new Counter(); }
             return instance;
         }
         public void AddOne()
