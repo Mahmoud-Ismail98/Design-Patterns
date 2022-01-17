ce7efdf7a8cfd9259f936bd6df6b084e92a68228
Mahmoud-Koraim <m.koraim77@gmail.com>
2022-01-17 19:50:55 +0200

    Singleton pattern implementation

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DesignPatterns.singleton;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = Counter.GetInstance();
+            Counter counter2 = Counter.GetInstance();
+            counter1.AddOne();
+            counter2.AddOne();
+
+            Console.WriteLine("counter 1:" + counter1.Count.ToString());
+            Console.WriteLine("counter 2:" + counter2.Count.ToString());
+            Console.WriteLine();
+
+            counter1.AddOne();
+            Console.WriteLine("counter 1:" + counter1.Count.ToString());
+            Console.WriteLine("counter 2:" + counter2.Count.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Singleton/Counter.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DesignPatterns.singleton
+{
+    public class Counter
+    {
+        public int Count = 0;
+        private static Counter instance = new Counter();
+        private Counter()
+        {
+
+        }
+        public static Counter GetInstance()
+        {
+            return instance;
+        }
+        public void AddOne()
+        {
+            Count++;
+        }
+    }
+}
